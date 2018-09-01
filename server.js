df9483519bc23f99d9bea2353cb3e6a49be278cb
Katherine Smith <katherine.lo.smith@gmail.com>
2018-08-31 17:57:34 -0700

    Test server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const pg = require('pg');
+const superagent = require('superagent');
+
+let app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
+const PORT = process.env.PORT;
+
+const CONSTRING = process.env.DATABASE_URL;
+// const CONSTRING = 'postgres://localhost:5432/books_app';
+
+let client = new pg.Client(CONSTRING);
+client.connect();
+client.on('error', err => {
+  console.error(err);
+});
+
+app.get('/', (request, response) => {
+  response.render('master');
+});
+
+app.listen( PORT, () => console.log('Server Up on ', PORT) );
